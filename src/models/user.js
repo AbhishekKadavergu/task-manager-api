8dfe89ba5a09f2acfdca9740a4d4660ac10d3934
abhishekkadavergu <abhishekkadavergu@gmail.com>
2022-05-05 11:15:30 +0530

    Some user.js file changes

--- src/models/user.js
+++ src/models/user.js
@@ -10,7 +10,7 @@ const userSchema = mongoose.Schema({
         required: true,
         trim: true,
     },
-    email:{
+    email: {
         type: String,
         required: true,
         trim: true,
@@ -20,8 +20,8 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
         trim: true,
-        validate: function(value){
-            if(value.toLowerCase().includes('password')){
+        validate: function(value) {
+            if (value.toLowerCase().includes('password')) {
                 throw new Error("Password cannot contain word password")
             }
         }
@@ -31,16 +31,15 @@ const userSchema = mongoose.Schema({
         default: 0,
     },
     tokens: [{
-        token:{
+        token: {
             type: String,
             required: true
         }
     }],
-    avatar:{
+    avatar: {
         type: Buffer
     }
-},
-{timestamps: true})
+}, { timestamps: true })
 
 userSchema.virtual('tasks', {
     ref: 'Task',
@@ -48,7 +47,7 @@ userSchema.virtual('tasks', {
     foreignField: 'owner'
 })
 
-userSchema.methods.toJSON = function(){
+userSchema.methods.toJSON = function() {
     const user = this
     const userObject = user.toObject()
     delete userObject.password
@@ -56,44 +55,44 @@ userSchema.methods.toJSON = function(){
     return userObject
 }
 
-userSchema.methods.generateAuthTokenAndSave = async function(){
+userSchema.methods.generateAuthTokenAndSave = async function() {
     const user = this
     console.log(user._id)
-    const token = jwt.sign({id:user._id.toString()}, process.env.JWT_SECRET)
-    user.tokens = user.tokens.concat({token})
+    const token = jwt.sign({ id: user._id.toString() }, process.env.JWT_SECRET)
+    user.tokens = user.tokens.concat({ token })
     await user.save()
     return token
 
 }
 
-userSchema.statics.findUserByCredentials = async function(email, password){
+userSchema.statics.findUserByCredentials = async function(email, password) {
     console.log("Find user")
-    const user = await User.findOne({email})
-    if(!user){
+    const user = await User.findOne({ email })
+    if (!user) {
         throw new Error("Invalid credentials")
     }
-    const userExist = await bcrypt.compare(password, user.password)   
-    if(!userExist) {
+    const userExist = await bcrypt.compare(password, user.password)
+    if (!userExist) {
         throw new Error("Invalid credentials")
     }
     return user
 
 }
 
-userSchema.pre('save', async function(next){
+userSchema.pre('save', async function(next) {
     console.log("Before save")
     const user = this
     if (!user.isModified('password')) return next();
 
-    const pwd = await bcrypt.hash(user.password, 8)  
-    user.password = pwd    
+    const pwd = await bcrypt.hash(user.password, 8)
+    user.password = pwd
     next()
 })
 
 //Delete tasks when user is deleted
-userSchema.pre('remove', async function(next){
+userSchema.pre('remove', async function(next) {
     const user = this
-    await Task.deleteMany({owner: user._id})
+    await Task.deleteMany({ owner: user._id })
     next()
 })
 
